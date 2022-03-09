1257e5dfe034a3e9414af3d40051a001b2eff652
Molaycomp <62835174+Molaycomp@users.noreply.github.com>
2022-03-09 08:03:17 +0530

    Cloud database connection

--- src/db/conn.js
+++ src/db/conn.js
@@ -1,7 +1,18 @@
 const mongoose=require("mongoose")
 
-mongoose.connect(process.env.DB_CONNECT)
+const db="mongodb+srv://molay:molaygiri@cluster0.26kgb.mongodb.net/projecdbb?retryWrites=true&w=majority";
+
+mongoose.connect(db,{
+    useNewUrlParser : true,
+    
+})
 .then( ()=>console.log("connection successful.....") )
 .catch((err)=> console.log(err));
 
+
+/*
+mongoose.connect(process.env.DB_CONNECT)
+.then( ()=>console.log("connection successful.....") )
+.catch((err)=> console.log(err));
+*/
 
